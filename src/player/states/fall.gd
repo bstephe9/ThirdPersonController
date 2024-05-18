b6f5ef4089017c9457e5c7d95d1b58e2f0a12025
bstephe9 <tun78906@temple.edu>
2024-05-18 17:57:31 -0400

    fix: Fix fall state not switching to walk state
    Fixed by assigning the Fall node's walk state (which I forgot).

--- src/player/states/fall.gd
+++ src/player/states/fall.gd
@@ -3,13 +3,13 @@ extends PlayerState
 @export
 var idle_state: State
 @export
-var move_state: State
+var walk_state: State
 
 func process_physics(delta: float) -> State:
 	super(delta)
 	
 	if parent.is_on_floor():
 		if input_dir != Vector2.ZERO:
-			return move_state
+			return walk_state
 		return idle_state
 	return null
